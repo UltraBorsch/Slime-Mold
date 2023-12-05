63bf9cf8228c71e4ffc61f71441eb4393b3ea54c
Joshua-M1 <jsmorency@hotmail.com>
2023-12-05 03:07:10 -0500

    changed how rendering works. implemented agents/trailmap.

--- Slime Mold/Assets/Scripts/C#/ComputeHelper.cs
+++ Slime Mold/Assets/Scripts/C#/ComputeHelper.cs
@@ -9,7 +9,7 @@ public static class ComputeHelper {
         return new Vector3Int((int)x, (int)y, (int)z);
     }
 
-    // Convenience method for dispatching a compute shader.
+    // Convenience method for dispatching a compute slimeSim.
     // It calculates the number of thread groups based on the number of iterations needed.
     public static void Run(ComputeShader cs, int numIterationsX, int numIterationsY = 1, int numIterationsZ = 1, int kernelIndex = 0) {
         Vector3Int threadGroupSizes = GetThreadGroupSizes(cs, kernelIndex);
@@ -17,5 +17,28 @@ public static class ComputeHelper {
         int numGroupsY = Mathf.CeilToInt(numIterationsY / (float)threadGroupSizes.y);
         int numGroupsZ = Mathf.CeilToInt(numIterationsZ / (float)threadGroupSizes.y);
         cs.Dispatch(kernelIndex, numGroupsX, numGroupsY, numGroupsZ);
+    }
+
+    public static void CreateStructuredBuffer<T>(ref ComputeBuffer buffer, int count) {
+        int stride = System.Runtime.InteropServices.Marshal.SizeOf(typeof(T));
+        bool createNewBuffer = buffer == null || !buffer.IsValid() || buffer.count != count || buffer.stride != stride;
+        if (createNewBuffer) {
+            Release(buffer);
+            buffer = new ComputeBuffer(count, stride);
+        }
+    }
+
+    public static void CreateStructuredBuffer<T>(ref ComputeBuffer buffer, T[] data) {
+        CreateStructuredBuffer<T>(ref buffer, data.Length);
+        buffer.SetData(data);
+    }
+
+    /// Releases supplied buffer/s if not null
+    public static void Release(params ComputeBuffer[] buffers) {
+        for (int i = 0; i < buffers.Length; i++) {
+            if (buffers[i] != null) {
+                buffers[i].Release();
+            }
+        }
     }
 }
--- Slime Mold/Assets/Scripts/C#/Controller.cs
+++ Slime Mold/Assets/Scripts/C#/Controller.cs
@@ -4,30 +4,67 @@ using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 
 public class Controller : MonoBehaviour {
-    public int width, height;
-    public ComputeShader shader;
+    public int width, height, numOfAgents;
+    public float agentSpeed;
+    public ComputeShader slimeSim, agentDisplay;
+    private ComputeBuffer agentBuffer = null;
+    public MeshRenderer agentRenderer;
 
-    public RenderTexture texture;
+    public RenderTexture trailMap;
 
-    private void OnRenderImage(RenderTexture src, RenderTexture dest) {
-        if (texture == null) {
-            texture = new(width, height, 0) {
-                enableRandomWrite = true,
-                graphicsFormat = GraphicsFormat.R16G16B16A16_SFloat,
-                autoGenerateMips = false,
-                filterMode = FilterMode.Bilinear,
-                wrapMode = TextureWrapMode.Clamp
-            };
-            texture.Create();
+    public struct Agent {
+        public Vector2 position;
+        public Vector2 direction;
+
+        public Agent(Vector2 position, Vector2 direction) {
+            this.position = position;
+            this.direction = direction;
         }
-
-        shader.SetInt("width", width);
-        shader.SetInt("height", height);
-        shader.SetTexture(0, "Texture", texture);
-        ComputeHelper.Run(shader, width, height);
-
-        Graphics.Blit(texture, dest);
     }
 
+    private void CreateAgents(Agent[] arr) {
+        for (int i = 0; i < arr.Length; i++) {
+            Vector2 position = new(width / 2, height / 2);
+            float angle = Random.Range(0f, 1f) * 2f * Mathf.PI;
+            Vector2 direction = new(Mathf.Cos(angle), Mathf.Sin(angle));
+            arr[i] = new(position, direction);
+        }
+    }
 
+    private void Start() {
+        trailMap = new(width, height, 0) {
+            enableRandomWrite = true,
+            graphicsFormat = GraphicsFormat.R16G16B16A16_SFloat,
+            autoGenerateMips = false,
+            filterMode = FilterMode.Bilinear,
+            wrapMode = TextureWrapMode.Clamp
+        };
+        trailMap.Create();
+        agentRenderer.material.mainTexture = trailMap;
+
+        Agent[] agents = new Agent[numOfAgents];
+        CreateAgents(agents);
+        ComputeHelper.CreateStructuredBuffer(ref agentBuffer, agents);
+
+        slimeSim.SetBuffer(0, "agents", agentBuffer);
+        slimeSim.SetInt("width", width);
+        slimeSim.SetInt("height", height);
+        slimeSim.SetInt("numAgents", numOfAgents);
+        slimeSim.SetFloat("PI", Mathf.PI);
+        slimeSim.SetFloat("moveSpeed", agentSpeed);
+        slimeSim.SetTexture(0, "TrailMap", trailMap);
+    }
+
+    private void FixedUpdate() {
+        slimeSim.SetFloat("deltaTime", Time.deltaTime);
+        RunSimulation();
+    }
+
+    void OnDestroy() {
+        ComputeHelper.Release(agentBuffer);
+    }
+
+    public void RunSimulation() {
+        ComputeHelper.Run(slimeSim, numOfAgents);
+    }
 }
